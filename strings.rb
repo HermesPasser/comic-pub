7cbc4c8913b75d27605381693202bbb42b65fa9b
Hermes Passer <dsldouglas@hotmail.com>
2021-07-21 12:03:17 -0300

    update the templates from strings.rb
    Removed example page since we have the writer will create them for us.
    Add ids to some elements in the manifest to the writer known from where
    place the metadata.
    Add template view to place the comic images
    The default main page is rem

--- strings.rb
+++ strings.rb
@@ -11,13 +11,13 @@ $temp_content_opf_content = <<END
 <?xml version="1.0" encoding="UTF-8" ?>
 <package version="3.0" unique-identifier="uid" xmlns="http://www.idpf.org/2007/opf">
  	<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
-		<dc:title>add</dc:title>
+		<dc:title id="cpub-title">add</dc:title>
 		<dc:language>en-US</dc:language>
-		<dc:contributor id="contributor">add</dc:contributor>
-		<dc:creator>add author</dc:creator>
+		<dc:contributor id="contributor">Comicpub</dc:contributor>
+		<dc:creator>Comicpub</dc:creator>
 		<dc:identifier id="uid">add</dc:identifier>
 
-		<meta property="dcterms:modified">2021-07-12T18:18:29Z</meta>
+		<meta id="cpub-modate" property="dcterms:modified">add</meta>
 		<meta name="fixed-layout" content="true" />
 		<meta name="book-type" content="comic" />
 		<meta name="primary-writing-mode" content="horizontal-rl" />
@@ -31,16 +31,14 @@ $temp_content_opf_content = <<END
 
 	<manifest>
 	    <item id="toc" 			href="toc.xhtml" media-type="application/xhtml+xml" properties="nav" />
-	    <item id="chapter_1" 	href="comic/chapter_1.xhtml" media-type="application/xhtml+xml" />
 	    <item id="main_css" 	href="css/main.css" media-type="text/css" />
 	</manifest>
 
 	<spine>
-		<itemref idref="toc" />
-	    <itemref idref="chapter_1" />
 	</spine>
 </package>
 END
+
 # about <dc:identifier>
 # 	must have the same id in the meta inside toc.ncx
 #	https://www.mobileread.com/forums/showthread.php?t=314516
@@ -48,9 +46,6 @@ END
 
 # NOTES:
 # cover should be add in the spine before the other stuff, maybe?
-# to add a toc 
-#	sprine <itemref idref="comic/toc" />
-#	mainfest <item id="toc" href="toc.xhtml" media-type="application/xhtml+xml" properties="nav" />
 # on the spine maybe this linerar and page-spread are important to render the pages correctly (it was in a kcc generated epub)
 # <itemref idref="chapter1" linear="yes" properties="page-spread-right" />
 # <itemref idref="chapter2" linear="yes" properties="page-spread-left" />
@@ -70,7 +65,6 @@ $temp_toc_content = <<END
         <nav id="toc" epub:type="toc">
             <h1 class="frontmatter">Table of Contents</h1>
             <ol class="contents">
-                <li><a href="comic/chapter_1.xhtml">Chapter 1</a></li>
             </ol>
         </nav>
     </body>
@@ -80,15 +74,15 @@ END
 # <!-- if we ever need to use css in this... -->
 # <!-- <link href="template.css" rel="stylesheet" type="text/css" /> -->
 
-$place_holder_main_content = <<END
-<?xml version="1.0" encoding="utf-8"?>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+$empty_img_html = <<END
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="en" lang="en">
     <head>
-        <title>1_hello.xhtml</title>
+        <title></title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <img src="" />
     </body>
 </html> 
 END
-# to ref a css in the file <link> like any ordinary html code
